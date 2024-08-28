854b495983133ba305f12e18b8428d6d280cc5f3
David Burrows <David.Burrows@noaa.gov>
2024-08-28 13:09:42 -0400

    add/update files to build prepobs dev/gfsv17 on Gaea C5

---
+++ modulefiles/fit2obs_gaea.lua
@@ -0,0 +1,15 @@
+help([[
+Build environment for fit2obs on Gaea
+]])
+
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
+
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
+
+load("fit2obs_common")
+
+whatis("Description: fit2obs environment on Gaea with Intel Compilers")
