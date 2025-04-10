92bb8ff89410e4caa390faaa5a8408a57ab95fde
kate.friedman <kate.friedman@noaa.gov>
2025-04-10 14:32:47 +0000

    COM variable updates for IN and OUT
    Rename/replace these variables:
    - COMLOX -> COMOUT_FITX
    - COM_INA/COMLIC -> COMIN_ANALYSIS
    - COM_INF/COMLICF -> COMIN_HISTORY
    - COMPRP/COM_PRP -> COMIN_OBS
    - COMPRP -> deleted in ush/ACprof
    - COMDAY -> LOGDIR
    
    Refs #36

--- ush/runfits.sh
+++ ush/runfits.sh
@@ -16,7 +16,7 @@ export FIT_DIR=$ARCDIR/fits  ; mkdir -p $FIT_DIR
 export HORZ_DIR=$ARCDIR/horiz; mkdir -p $HORZ_DIR
 
 export DATA=$TMPDIR/$exp.$CDATE.fit2obstmp; rm -rf $DATA; mkdir -p $DATA
-export COMLOX=$DATA/fitx; mkdir -p $COMLOX
+export COMOUT_FITX=$DATA/fitx; mkdir -p $COMOUT_FITX
 
 echo "echo err_chk">$DATA/err_chk; chmod 755 $DATA/err_chk
 echo "echo postmsg">$DATA/postmsg; chmod 755 $DATA/postmsg
