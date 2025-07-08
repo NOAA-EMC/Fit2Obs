8d1ea648644428d5201376a05fbe977a41125064
Taylor Roper <taylor.roper@noaa.gov>
2025-07-08 18:07:11 +0000

    Update cloud to use spack-stack 1.9.1 (#45)

--- modulefiles/fit2obs_noaacloud.lua
+++ modulefiles/fit2obs_noaacloud.lua
@@ -2,15 +2,16 @@ help([[
 Build environment for fit2obs on NOAA cloud
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", os.getenv("spack_stack_mod_path"))
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
-
-load("gnu")
-load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("gnu", gcc_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 load("fit2obs_common")
+
+setenv("CC","/apps/oneapi/mpi/latest/bin/mpiicc")
+setenv("CXX","/apps/oneapi/mpi/latest/bin/mpiicpc")
+setenv("FC","/apps/oneapi/mpi/latest/bin/mpiifort")
 
 whatis("Description: fit2obs environment on NOAA cloud")
